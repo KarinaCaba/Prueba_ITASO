a7eeeff86072dd96aea427545ec673f856448af1
Luis Blanco <slam46507@live.com.mx>
2023-07-06 18:11:11 -0600

    some fixes

--- client/src/pages/404/404.jsx
+++ client/src/pages/404/404.jsx
@@ -7,28 +7,28 @@ import fondo_calcu_06 from "./Images/fondo_calcu-06.png";
 const NotFound = () => {
 	return (
 		<div className="NotFound">
-			<section class="container-fluid pt-5">
-				<div class="row sections">
-					<div class="center">
+			<section className="container-fluid pt-5">
+				<div className="row sections">
+					<div className="center">
 						<img src={logo2} width="40%" alt="Logo itaso" />
 					</div>
 				</div>
-				<section class="row pt-5 sections">
-					<div class="prueba col text-center align-middle">
+				<section className="row pt-5 sections">
+					<div className="prueba col text-center align-middle">
 						<h2 className="mb-3">404 Not found</h2>
 						<h3>La pagina que buscas no existe</h3>
 
 						<Link
 							role="button"
-							className="btn btn-light btn-lg mt-3"
+							className="btn btn-dark btn-lg mt-3 py-2 px-4"
 							to="/">
 							Inicio
 						</Link>
 					</div>
-					<div class="col text-center align-middle">
-						<div class="banner_img">
+					<div className="col text-center align-middle">
+						<div className="banner_img">
 							<img
-								class="img"
+								className="img"
 								src={fondo_calcu_06}
 								width="200"
 								height="200"
@@ -37,17 +37,17 @@ const NotFound = () => {
 						</div>
 					</div>
 				</section>
-				<div class="burbujas">
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
-					<div class="burbuja"></div>
+				<div className="burbujas">
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
+					<div className="burbuja"></div>
 				</div>
 			</section>
 		</div>
