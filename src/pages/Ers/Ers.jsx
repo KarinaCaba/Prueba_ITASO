13c32c98a6b6cef922c3ce51fac652a4f85c36d9
EmilianoPC <emilianogarcia4550@yahoo.com.mx>
2024-08-04 16:45:40 -0600

    Eventos responsivos

--- src/pages/Ers/Ers.jsx
+++ src/pages/Ers/Ers.jsx
@@ -11,10 +11,11 @@ const Ers = () => {
 
 
   return (
-    <div className="ers">
+   /*<div className="ers">
       <div className="sect-name">
         <h1 className="primary-title">Eventos, reuniones y sesiones</h1>
       </div>
+
 
       <div className="row container">
         <img
@@ -36,9 +37,84 @@ const Ers = () => {
         />
         <Link className="btn-profile grid-item">Ver evento</Link>
       </div>
+    </div>*/
+
+/* segunda versión
+    <div className="ers">
+    <div className="sect-name">
+      <h1 className="primary-title">Eventos, reuniones y sesiones</h1>
     </div>
+  
+    <div className="row container">
+      <div className="col grid-item">
+        <img
+          src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-163 (1).jpg"
+          alt=""
+          className="grid-image"
+        />
+        <Link className="btn-profile">Ver evento</Link>
+      </div>
+      <div className="col grid-item">
+        <img
+          src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-64.jpg"
+          alt=""
+          className="grid-image"
+        />
+        <Link className="btn-profile">Ver evento</Link>
+      </div>
+      <div className="col grid-item">
+        <img
+          src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-93.jpg"
+          alt=""
+          className="grid-image"
+        />
+        <Link className="btn-profile">Ver evento</Link>
+      </div>
+    </div>
+  </div>
+  */
+
+  <div className="page-container">
+  <div className="ers">
+    <div className="sect-name">
+      <h1 className="primary-title">Eventos, reuniones y sesiones</h1>
+    </div>
+    <div className="row container">
+      <div className="col grid-item">
+        <img
+          src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-163 (1).jpg"
+          alt=""
+          className="grid-image"
+        />
+        <Link className="btn-profile">Ver evento</Link>
+      </div>
+      <div className="col grid-item">
+        <img
+          src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-64.jpg"
+          alt=""
+          className="grid-image"
+        />
+        <Link className="btn-profile">Ver evento</Link>
+      </div>
+      <div className="col grid-item">
+        <img
+          src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-93.jpg"
+          alt=""
+          className="grid-image"
+        />
+        <Link className="btn-profile">Ver evento</Link>
+      </div>
+    </div>
+  </div>
+  <footer className="footer">
+   
+  </footer>
+</div>
+
+
   );
 };
 
 export default Ers;
+
 
