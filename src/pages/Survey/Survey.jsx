6665ff6fdb605de8f41ac691bc750f157a858c37
EmilianoPC <emilianogarcia4550@yahoo.com.mx>
2024-08-05 12:50:35 -0600

    Encuestas responsivas

--- src/pages/Survey/Survey.jsx
+++ src/pages/Survey/Survey.jsx
@@ -7,6 +7,8 @@ import { useDocumentTitle } from "../../hooks/setdocumenttitle";
 const Survey = () => {
   useDocumentTitle("Encuestas - ITASO");
   return (
+    
+    /*
     <div className="survey">
       <div className="sect-name">
         <h1 className="primary-title">Encuestas</h1>
@@ -32,7 +34,47 @@ const Survey = () => {
         />
         <Link className="btn-profile grid-item">Ver evento</Link>
       </div>
+    </div>*/
+
+    <div className="page-container">
+    <div className="survey">
+      <div className="sect-name">
+        <h1 className="primary-title">Encuestas</h1>
+      </div>
+      <div className="row container">
+        <div className="col grid-item">
+          <img
+            src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-163 (1).jpg"
+            alt=""
+            className="grid-image"
+          />
+          <Link className="btn-profile">Ver evento</Link>
+        </div>
+        <div className="col grid-item">
+          <img
+            src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-64.jpg"
+            alt=""
+            className="grid-image"
+          />
+          <Link className="btn-profile">Ver evento</Link>
+        </div>
+        <div className="col grid-item">
+          <img
+            src="/assets/rallys/VI_Rally_Bienestar_Integral_ITASO-MX_RS_2205-93.jpg"
+            alt=""
+            className="grid-image"
+          />
+          <Link className="btn-profile">Ver evento</Link>
+        </div>
+      </div>
     </div>
+    <footer className="footer">
+     
+    </footer>
+  </div>
+  
+
+
   );
 };
 
